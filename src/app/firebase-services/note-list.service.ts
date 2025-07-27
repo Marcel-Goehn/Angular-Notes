e1b2556f1af868da693d8cbfbcc2d8c0440d31b4
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-27 16:45:59 +0200

    implemented collectionData to show the fetched Data in the html

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
-  constructor() { }
+  firestore = inject(Firestore);
+  items$ = collectionData(this.getNotesRef());
+
+  getTrashRef() {
+    return collection(this.firestore, 'trash');
+  }
+
+
+  getNotesRef() {
+    return collection(this.firestore, 'notes');
+  }
+
+
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(this.firestore, colId, docId);
+  }
 }
