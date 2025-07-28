bc70b9f762ee22907f1bff00b83b97093171f598
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-28 20:56:53 +0200

    created a method to add notes to the firestore database

--- src/app/add-note-dialog/add-note-dialog.component.ts
+++ src/app/add-note-dialog/add-note-dialog.component.ts
@@ -13,10 +13,10 @@ import { FormsModule } from '@angular/forms';
 })
 export class AddNoteDialogComponent {
   @Output() addDialogClosed: EventEmitter<boolean> = new EventEmitter();
-  title = "";
-  description = "";
+  title: string = "";
+  description: string = "";
 
-  constructor(public noteService: NoteListService){}
+  constructor(private noteService: NoteListService){}
 
   closeDialog() {
     this.title = "";
@@ -25,6 +25,13 @@ export class AddNoteDialogComponent {
   }
 
   addNote(){
+    let note: Note = {
+      type: 'note',
+      title: this.title,
+      content: this.description,
+      marked: false
+    }
+    this.noteService.addNote(note);
     //beachte das closeDialog() zum Schluss kommt, denn es leert die Variablen
     this.closeDialog();
   }
