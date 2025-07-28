ab788efa1c08d568ac8c37972666a01efc99169f
Marcel Göhn <marcelgoehn@googlemail.com>
2025-07-28 20:34:25 +0200

    added onSnapshot() to get realtime updates from the firestore database

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,13 +1,70 @@
 import { Injectable, inject } from '@angular/core';
-import { Firestore, collection, doc, collectionData, onSnapshot } from '@angular/fire/firestore';
+import { Firestore, collection, doc, collectionData, onSnapshot, addDoc } from '@angular/fire/firestore';
+import { Note } from '../interfaces/note.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NoteListService {
 
+  trashNotes: Note[] = [];
+  normalNotes: Note[] = [];
+
   firestore = inject(Firestore);
-  items$ = collectionData(this.getNotesRef());
+  unsubTrash;
+  unsubNote;
+
+  constructor() {
+    this.unsubTrash = this.subTrashList();
+    this.unsubNote = this.subNotesList();
+  }
+
+
+  async addNote(item: {}) {
+    await addDoc(this.getNotesRef(), item).catch(
+      (err) => console.error(err) 
+    ).then(
+      (docRef) => console.log("Document written with ID: ", docRef?.id)
+    )
+  }
+
+
+  ngOnDestroy() {
+    this.unsubTrash();
+    this.unsubNote();
+  }
+
+
+  subTrashList() {
+    return onSnapshot(this.getTrashRef(), (list) => {
+      this.trashNotes = [];
+      list.forEach(element => {
+        this.trashNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    });
+  }
+
+
+  subNotesList() {
+    return onSnapshot(this.getNotesRef(), (list) => {
+      this.normalNotes = [];
+      list.forEach(element => {
+        this.normalNotes.push(this.setNoteObject(element.data(), element.id));
+      })
+    });
+  }
+
+
+  setNoteObject(obj: any, id: string): Note {
+    return {
+      id: id || '',
+      type: obj.type || 'note',
+      title: obj.title || '',
+      content: obj.content || '',
+      marked: obj.marked || false
+    }
+  }
+
 
   getTrashRef() {
     return collection(this.firestore, 'trash');
@@ -19,7 +76,7 @@ export class NoteListService {
   }
 
 
-  getSingleDocRef(colId: string, docId: string) {
-    return doc(this.firestore, colId, docId);
-  }
+  // getSingleDocRef(colId: string, docId: string) {
+  //   return doc(this.firestore, colId, docId);
+  // }
 }
--- src/app/note-list/note-list.component.ts
+++ src/app/note-list/note-list.component.ts
@@ -6,7 +6,6 @@ import { CommonModule } from '@angular/common';
 import { NoteComponent } from './note/note.component';
 
 
-
 @Component({
   selector: 'app-note-list',
   standalone: true,
@@ -14,13 +13,16 @@ import { NoteComponent } from './note/note.component';
   templateUrl: './note-list.component.html',
   styleUrl: './note-list.component.scss'
 })
+
 export class NoteListComponent {
   noteList: Note[] = [];
   favFilter: "all" | "fav" = "all";
   status: "notes" | "trash" = "notes";
 
-  constructor(public noteService: NoteListService) {
-    this.noteList = this.getDummyData()
+  constructor(private noteService: NoteListService) { }
+
+  getList(): Note[] {
+    return this.noteService.normalNotes;
   }
 
   changeFavFilter(filter:"all" | "fav"){
@@ -35,47 +37,4 @@ export class NoteListComponent {
       this.favFilter = "all";
     }
   }
-
-
-
-
-  getDummyData(): Note[] {
-    return [
-      {
-        id: "21sasd561dd4sdf",
-        type: "note",
-        title: "Block, Inline, and Inline-Block",
-        content: "https://www.youtube.com/watch?v=x_i2gga-sYg",
-        marked: true,
-      },
-      {
-        id: "25sd4f561w54sdf",
-        type: "note",
-        title: "css selector",
-        content: `kind p > b   (direktes kind) 
-        nachfahren p b  (alle nachfahren)
-        geschwister p ~ b (auf gleicher ebene ist VOR dem p ein b)`,
-        marked: true,
-      },
-      {
-        id: "54a4s6d546ff",
-        type: "note",
-        title: "aufräumen",
-        content: "Wohnzimmer saugen",
-        marked: false,
-      },
-      {
-        id: "2a35s4d654a6s4d",
-        type: "note",
-        title: "links",
-        content: `Reihenfolge: a:visited 
-        a:focus 
-        a:hover 
-        a:active
-        merkspruch: LoVe HAte`,
-        marked: true,
-      }
-    ];
-  }
-
 }
